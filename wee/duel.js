873cc06a09233f5be05e70ddbef450a445f7e8db
idontknowstuff <55608963+idontknowstuff@users.noreply.github.com>
2022-02-05 12:49:04 -0700

    Update duel.js
    Attached the words to the blocks, added timer. Working on why there is an error when block goes into dropzone

--- wee/duel.js
+++ wee/duel.js
@@ -1,6 +1,20 @@
 class Duel extends Phaser.Scene {
     constructor() {
         super("Duel");
+
+        this.block1;
+        this.block2;
+        this.block3;
+        this.block4;
+        this.block1text;
+        this.block2text;
+        this.block3text;
+        this.block4text;
+        this.time;
+        this.zone1;
+        this.zone2;
+        this.zone3;
+        this.zone4;
     }
 
     
@@ -12,6 +26,7 @@ class Duel extends Phaser.Scene {
         var EnemyHealth = 5;
         var playerprogress = 4;
         var enemyprogress = 1;
+        this.timer = 300;
 
         // background
         this.background = this.add.image(0, 0, "background");
@@ -32,7 +47,7 @@ class Duel extends Phaser.Scene {
         var playerY = this.mini(575 - playerprogress*100, 575);
         var text = this.add.text(970, enemyY, enemyprogress, {fontSize: '20px', fill: "#000000", stroke: '#000000', strokeThickness: 1});
         var text = this.add.text(920, playerY, playerprogress, {fontSize: '20px', fill: "#000000", stroke: '#000000', strokeThickness: 1});
-
+        this.time = this.add.text(350, 50, this.timer);
         // timer
         // const timerLabel = this.add.text(50,50, "45");
  
@@ -40,52 +55,70 @@ class Duel extends Phaser.Scene {
         this.add.image(0, 200, "WorkPlace").setOrigin(0,0);
         this.add.image(0, 100, "QuestionPlace").setOrigin(0,0);
 
-        // blocks
-        var block1 = this.add.image(575, 250, "blocks");
-        var block2 = this.add.image(575, 350, "blocks");
-        var block3 = this.add.image(575, 450, "blocks");
-        var block4 = this.add.image(575, 550, "blocks");
+        // blocks and sets name
+        this.block1 = this.add.image(575, 250, "blocks").setName("block1");
+        this.block2 = this.add.image(575, 350, "blocks").setName("block2");
+        this.block3 = this.add.image(575, 450, "blocks").setName("block3");
+        this.block4 = this.add.image(575, 550, "blocks").setName("block4");
+
+        // empty text variables for later inserting uses 
+        this.block1text = this.add.text(200,300, "");
+        this.block2text = this.add.text(200,300, "");
+        this.block3text = this.add.text(200,300, "");
+        this.block4text = this.add.text(200,300, "");
+
+        // set names for blocks
+        // block1.setName("block1");
+        // block2.setName("block2");
+        // block3.setName("block3");
+        // block4.setName("block4");
 
         // make the blocks draggable
-        block1.setInteractive();
-        block2.setInteractive();
-        block3.setInteractive();
-        block4.setInteractive();
-        this.input.setDraggable(block1);
-        this.input.setDraggable(block2);
-        this.input.setDraggable(block3);
-        this.input.setDraggable(block4);
+        this.block1.setInteractive();
+        this.block2.setInteractive();
+        this.block3.setInteractive();
+        this.block4.setInteractive();
+        this.input.setDraggable(this.block1);
+        this.input.setDraggable(this.block2);
+        this.input.setDraggable(this.block3);
+        this.input.setDraggable(this.block4);
+
+        this.input.on('dragstart', function (pointer, gameObject, dragX, dragY){
+            
+        });
 
         this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
 
             gameObject.x = dragX;
             gameObject.y = dragY;
 
+
         });
 
-        //  4 drop zones
-        var zone1 = this.add.zone(200, 250, 300, 300).setRectangleDropZone(100, 50);
-        var zone2 = this.add.zone(200, 350, 300, 400).setRectangleDropZone(100, 50);
-        var zone3 = this.add.zone(200, 450, 300, 500).setRectangleDropZone(100, 50);
-        var zone4 = this.add.zone(200, 550, 300, 600).setRectangleDropZone(100, 50);
+        //  4 drop zones and sets the names
+        this.zone1 = this.add.zone(200, 250, 300, 300).setRectangleDropZone(100, 50).setName("zone1");
+        this.zone2 = this.add.zone(200, 350, 300, 400).setRectangleDropZone(100, 50).setName("zone2");
+        this.zone3 = this.add.zone(200, 450, 300, 500).setRectangleDropZone(100, 50).setName("zone3");
+        this.zone4 = this.add.zone(200, 550, 300, 600).setRectangleDropZone(100, 50).setName("zone4");
 
         //  Just a visual display of the drop zone
         var graphics = this.add.graphics();
         graphics.lineStyle(2, 0xffff00);
-        graphics.strokeRect(zone1.x - zone1.input.hitArea.width / 2, zone1.y - zone1.input.hitArea.height / 2, zone1.input.hitArea.width, zone1.input.hitArea.height);
-        graphics.strokeRect(zone2.x - zone2.input.hitArea.width / 2, zone2.y - zone2.input.hitArea.height / 2, zone2.input.hitArea.width, zone2.input.hitArea.height);
-        graphics.strokeRect(zone3.x - zone3.input.hitArea.width / 2, zone3.y - zone3.input.hitArea.height / 2, zone3.input.hitArea.width, zone3.input.hitArea.height);
-        graphics.strokeRect(zone4.x - zone4.input.hitArea.width / 2, zone4.y - zone4.input.hitArea.height / 2, zone4.input.hitArea.width, zone4.input.hitArea.height);
+        graphics.strokeRect(this.zone1.x - this.zone1.input.hitArea.width / 2, this.zone1.y - this.zone1.input.hitArea.height / 2, this.zone1.input.hitArea.width, this.zone1.input.hitArea.height);
+        graphics.strokeRect(this.zone2.x - this.zone2.input.hitArea.width / 2, this.zone2.y - this.zone2.input.hitArea.height / 2, this.zone2.input.hitArea.width, this.zone2.input.hitArea.height);
+        graphics.strokeRect(this.zone3.x - this.zone3.input.hitArea.width / 2, this.zone3.y - this.zone3.input.hitArea.height / 2, this.zone3.input.hitArea.width, this.zone3.input.hitArea.height);
+        graphics.strokeRect(this.zone4.x - this.zone4.input.hitArea.width / 2, this.zone4.y - this.zone4.input.hitArea.height / 2, this.zone4.input.hitArea.width, this.zone4.input.hitArea.height);
         
 
         this.input.on('dragenter', function (pointer, gameObject, dropZone) {
 
             graphics.clear();
             graphics.lineStyle(2, 0x00ffff);
+            console.log(dropZone.name);
             graphics.strokeRect(zone1.x - zone1.input.hitArea.width / 2, zone1.y - zone1.input.hitArea.height / 2, zone1.input.hitArea.width, zone1.input.hitArea.height);
-            graphics.strokeRect(zone2.x - zone2.input.hitArea.width / 2, zone2.y - zone2.input.hitArea.height / 2, zone2.input.hitArea.width, zone2.input.hitArea.height);
-            graphics.strokeRect(zone3.x - zone3.input.hitArea.width / 2, zone3.y - zone3.input.hitArea.height / 2, zone3.input.hitArea.width, zone3.input.hitArea.height);
-            graphics.strokeRect(zone4.x - zone4.input.hitArea.width / 2, zone4.y - zone4.input.hitArea.height / 2, zone4.input.hitArea.width, zone4.input.hitArea.height);
+            graphics.strokeRect(this.zone2.x - this.zone2.input.hitArea.width / 2, this.zone2.y - this.zone2.input.hitArea.height / 2, this.zone2.input.hitArea.width, this.zone2.input.hitArea.height);
+            graphics.strokeRect(this.zone3.x - this.zone3.input.hitArea.width / 2, this.zone3.y - this.zone3.input.hitArea.height / 2, this.zone3.input.hitArea.width, this.zone3.input.hitArea.height);
+            graphics.strokeRect(this.zone4.x - this.zone4.input.hitArea.width / 2, this.zone4.y - this.zone4.input.hitArea.height / 2, this.zone4.input.hitArea.width, this.zone4.input.hitArea.height);
 
         });
 
@@ -93,10 +126,10 @@ class Duel extends Phaser.Scene {
 
             graphics.clear();
             graphics.lineStyle(2, 0xffff00);
-            graphics.strokeRect(zone1.x - zone1.input.hitArea.width / 2, zone1.y - zone1.input.hitArea.height / 2, zone1.input.hitArea.width, zone1.input.hitArea.height);
-            graphics.strokeRect(zone2.x - zone2.input.hitArea.width / 2, zone2.y - zone2.input.hitArea.height / 2, zone2.input.hitArea.width, zone2.input.hitArea.height);
-            graphics.strokeRect(zone3.x - zone3.input.hitArea.width / 2, zone3.y - zone3.input.hitArea.height / 2, zone3.input.hitArea.width, zone3.input.hitArea.height);
-            graphics.strokeRect(zone4.x - zone4.input.hitArea.width / 2, zone4.y - zone4.input.hitArea.height / 2, zone4.input.hitArea.width, zone4.input.hitArea.height);
+            graphics.strokeRect(this.zone1.x - this.zone1.input.hitArea.width / 2, this.zone1.y - this.zone1.input.hitArea.height / 2, this.zone1.input.hitArea.width, this.zone1.input.hitArea.height);
+            graphics.strokeRect(this.zone2.x - this.zone2.input.hitArea.width / 2, this.zone2.y - this.zone2.input.hitArea.height / 2, this.zone2.input.hitArea.width, this.zone2.input.hitArea.height);
+            graphics.strokeRect(this.zone3.x - this.zone3.input.hitArea.width / 2, this.zone3.y - this.zone3.input.hitArea.height / 2, this.zone3.input.hitArea.width, this.zone3.input.hitArea.height);
+            graphics.strokeRect(this.zone4.x - this.zone4.input.hitArea.width / 2, this.zone4.y - this.zone4.input.hitArea.height / 2, this.zone4.input.hitArea.width, this.zone4.input.hitArea.height);
 
         });
 
@@ -105,7 +138,9 @@ class Duel extends Phaser.Scene {
             gameObject.x = dropZone.x;
             gameObject.y = dropZone.y;
 
-            console.log(gameObject);
+            console.log(gameObject.name);
+            console.log(dropZone.name);
+            console.log(dropZone)
 
             gameObject.input.enabled = true;
 
@@ -121,10 +156,10 @@ class Duel extends Phaser.Scene {
 
             graphics.clear();
             graphics.lineStyle(2, 0xffff00);
-            graphics.strokeRect(zone1.x - zone1.input.hitArea.width / 2, zone1.y - zone1.input.hitArea.height / 2, zone1.input.hitArea.width, zone1.input.hitArea.height);
-            graphics.strokeRect(zone2.x - zone2.input.hitArea.width / 2, zone2.y - zone2.input.hitArea.height / 2, zone2.input.hitArea.width, zone2.input.hitArea.height);
-            graphics.strokeRect(zone3.x - zone3.input.hitArea.width / 2, zone3.y - zone3.input.hitArea.height / 2, zone3.input.hitArea.width, zone3.input.hitArea.height);
-            graphics.strokeRect(zone4.x - zone4.input.hitArea.width / 2, zone4.y - zone4.input.hitArea.height / 2, zone4.input.hitArea.width, zone4.input.hitArea.height);
+            graphics.strokeRect(this.zone1.x - this.zone1.input.hitArea.width / 2, this.zone1.y - this.zone1.input.hitArea.height / 2, this.zone1.input.hitArea.width, this.zone1.input.hitArea.height);
+            graphics.strokeRect(this.zone2.x - this.zone2.input.hitArea.width / 2, this.zone2.y - this.zone2.input.hitArea.height / 2, this.zone2.input.hitArea.width, this.zone2.input.hitArea.height);
+            graphics.strokeRect(this.zone3.x - this.zone3.input.hitArea.width / 2, this.zone3.y - this.zone3.input.hitArea.height / 2, this.zone3.input.hitArea.width, this.zone3.input.hitArea.height);
+            graphics.strokeRect(this.zone4.x - this.zone4.input.hitArea.width / 2, this.zone4.y - this.zone4.input.hitArea.height / 2, this.zone4.input.hitArea.width, this.zone4.input.hitArea.height);
 
         });
 
@@ -136,11 +171,23 @@ class Duel extends Phaser.Scene {
         // loads question
         this.loadquestion();
         
+        // setInterval(this.countdown, 1000);
+        setInterval(this.countdown.bind(this), 1000);
+        // console.log(this.timer);
 
     }
 
     update(){
+        this.block1text.setPosition(this.block1.x, this.block1.y);
+        this.block2text.setPosition(this.block2.x, this.block2.y);
+        this.block3text.setPosition(this.block3.x, this.block3.y);
+        this.block4text.setPosition(this.block4.x, this.block4.y);
+    }
 
+    countdown(){
+        this.timer -= 1;
+        this.time.setText("Time left: " + this.timer);
+        // console.log(this.timer);
     }
 
     clickBackButton(){
@@ -184,8 +231,19 @@ class Duel extends Phaser.Scene {
             .then(response => response.json())
             .then(data => {
                 var style = { font: "20px Arial", fill: "#00ff00", align: "center", wordWrap: { width: 600 } };
-                var text = this.add.text(25,125,data.questions[1].question, style);
+                var text = this.add.text(25,125,data.questions[0].question, style);
                 text.setColor("#00ff00");
+
+                // insert the data into the empty string variables
+                this.block1text.setText(data.questions[0].blocks[0]);
+                this.block2text.setText(data.questions[0].blocks[1]);
+                this.block3text.setText(data.questions[0].blocks[2]);
+                this.block4text.setText(data.questions[0].blocks[3]);
+
+                // var text = this.add.text(200,400,data.questions[1].blocks[1], style);
+                // var text = this.add.text(200,500,data.questions[1].blocks[2], style);
+                // var text = this.add.text(200,550,data.questions[1].blocks[3], style);
+
 
             })
             .catch(error => console.log(error));
